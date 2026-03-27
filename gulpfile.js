1353767bc7c32eba59c64d18b4068ce70ac7453c
Kissraii <kisslerraissa75@gmail.com>
2026-03-27 13:33:27 -0300

    setup do projeto

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
-function build(cb) {
-    console.log('Build funcionando!');
-    cb();
-}
-
 function styles() {
-    return gulp.src('.src/styles/*.scss')
+    return gulp.src('./src/styles/**/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(gulp.dest('./dist/css'));
+        .pipe(gulp.dest('./build/css'));
 }
 
-exports.build = build;
-exports.default = styles;
+
+exports.build = styles;
+
+// Observa mudanças
+exports.watch =function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+}
