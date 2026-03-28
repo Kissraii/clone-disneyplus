f671ef60a11e7db2f8340faf87fae8e3e2726a8c
Kissraii <kisslerraissa75@gmail.com>
2026-03-28 15:26:38 -0300

    atualizei

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,30 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const imagemin =require('gulp-imagemin');
+const imagemin = require('gulp-imagemin');
 
+// Compila Sass
 function styles() {
     return gulp.src('./src/styles/**/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest('./dist/css'));
 }
 
-function images(){
+// Otimiza imagens
+function images() {
     return gulp.src('./src/images/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./dist/images'));
 }
 
+// Exportar tasks
+exports.styles = styles;
+exports.images = images;
 
-exports.default = gulp.parallel(styles,images);
+// Roda tudo junto
+exports.default = gulp.parallel(styles, images);
 
-
-// Observa mudanças
-exports.watch =function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
-}
+// Watch corrigido
+exports.watch = function () {
+    gulp.watch('./src/styles/**/*.scss', styles);
+    gulp.watch('./src/images/**/*', images);
+};
