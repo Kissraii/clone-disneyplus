9df930d26fac97804d5ae88c4696643b6aecaa19
Kissraii <kisslerraissa75@gmail.com>
2026-03-30 17:53:49 -0300

    Criação do Faq

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
-
+    const questions = document.querySelectorAll('[data-faq-question]');
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click',function(button){
@@ -13,7 +13,18 @@ document.addEventListener('DOMContentLoaded', function() {
             button.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta );
+    }
 })
+
+function abreOutFechaResposta(element){
+    const classe ='faq__questions__item--is-open';
+    const elementPai = element.target.parentNode;
+
+    elementPai.classList.toggle(classe);
+}
 
     function escondeTodasAbas(){
         const buttons = document.querySelectorAll('[data-tab-button]');
